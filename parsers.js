e3f0e8cb6642f7683859f9e17a7f63b4d1c06809
Bert Spaan <bert.spaan@gmail.com>
2022-03-17 18:30:06 +0100

    removed empty parser files

--- parsers.js
+++
@@ -1,5 +0,0 @@
-import * as loc from './parsers/loc.js'
-
-export {
-  loc
-}
--- parsers/loc.js
+++
@@ -1,9 +0,0 @@
-export function getMapCount (count = 1) {
-  // https://www.loc.gov/maps/?q=&fo=json
-}
-
-export function getRandomMap (count = 1) {
-  // "id": "http://www.loc.gov/item/75694770/",
-  // https://www.loc.gov/item/sanborn03376_001/manifest.json
-  return 'https://www.loc.gov/item/sanborn03376_001/manifest.json'
-}
